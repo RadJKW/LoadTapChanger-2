c20c412be3f9e96fb6a0eb960ec15a3550feb7c1
RadJKW <jwest@howard.com>
2022-10-19 08:24:48 -0500

    Leaving (per-model) configs here for example, they are never invoked

---
+++ PlcTagLibrary/Configurations/MicrologixPlcConfig.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PlcTagLibrary.Models;
+
+namespace PlcTagLibrary.Configurations;
+public class MicrologixPlcConfig : IEntityTypeConfiguration<MicrologixPlc>
+{
+    public void Configure(EntityTypeBuilder<MicrologixPlc> builder)
+    {
+
+        //builder.ToTable("MicrologixPlc")
+        //    .HasKey(e => e.PlcId)
+        //    .HasName("PrimaryKey_PlcId");
+
+        //builder.HasKey(e => e.PlcId);
+
+        //builder.Property(e => e.PlcId)
+        //    .ValueGeneratedOnAdd();
+
+        //builder.Property(e => e.Gateway)
+        //    .IsRequired()
+        //    .HasMaxLength(20);
+
+        //// if no name is provided then set the name to ('PLC' + 'PlcId')
+        //builder.Property(e => e.DefaultName)
+        //    .HasComputedColumnSql("'PLC-' +  [PlcId]")
+        //    .HasMaxLength(50);
+
+
+
+        //builder.Property(e => e.PlcType)
+
+        //    .HasMaxLength(50)
+        //    .HasConversion<string>();
+
+        //builder.Property(e => e.Protocol)
+
+        //    .HasMaxLength(50)
+        //    .HasConversion<string>();
+
+        //builder.Property(e => e.TimeoutSeconds);
+
+
+    }
+}
---
+++ PlcTagLibrary/Configurations/MicrologixTagConfig.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Formats.Asn1;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PlcTagLibrary.Models;
+
+namespace PlcTagLibrary.Configurations;
+public class MicrologixTagConfig : IEntityTypeConfiguration<MicrologixTag>
+{
+    public void Configure(EntityTypeBuilder<MicrologixTag> builder)
+
+    {
+        // not implemented 
+        throw new NotImplementedException();
+    }
+    //#region Entity Configuration...
+    //builder
+    //    .ToTable("MicrologixTag");
+    //builder
+    //    .HasKey(e => e.TagId)
+    //    .HasName("PrimaryKey_TagId");
+
+    //builder
+    //    .HasIndex(e => new { e.TagId, e.CustomName })
+    //    .IsUnique();
+    //#endregion
+
+    //#region Properties Configuration...
+    //builder.Property(e => e.TagId)
+    //    .ValueGeneratedOnAdd();
+
+    //builder.Property(e => e.CustomName)
+    //    .IsRequired()
+    //    .HasMaxLength(50);
+
+    //builder.Property(e => e.PlcDeviceId);
+
+    //builder.Property(e => e.ConfiguredName)
+    //    .IsRequired()
+    //    .HasMaxLength(50);
+
+    //builder.Property(e => e.TagType)
+    //    .IsRequired()
+    //    .HasMaxLength(50)
+    //    .HasConversion<string>();
+
+    //builder.Property(e => e.Value)
+    //    .HasDefaultValue(null);
+
+    //builder.HasOne(e => e.PlcDevice)
+    //    .WithMany(e => e.PlcTags)
+    //    .HasForeignKey(e => e.PlcDeviceId);
+    //#endregion
+}
+
