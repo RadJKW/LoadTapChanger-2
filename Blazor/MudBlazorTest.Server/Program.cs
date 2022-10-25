91f23142ab749bdc96c2ade46b4bd12d0070bff3
RadJKW <jwest@howard.com>
2022-10-25 10:27:55 -0500

    Added ability to consume MicrologixPlcs in Blazor;

--- Blazor/MudBlazorTest.Server/Program.cs
+++ Blazor/MudBlazorTest.Server/Program.cs
@@ -4,7 +4,7 @@ using MudBlazor.Examples.Data;
 using MudBlazor.Services;
 using MudBlazorTest.Server.Data;
 using MudBlazorTest.Server.Services;
-using MudBlazorTest.Server.Services.Notifications;
+using MudBlazorTest.Server.Services.Base;
 using MudBlazorTest.Server.Startup;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +15,8 @@ builder.Services.AddControllers();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddHttpClient<IClient, Client>(options => options.BaseAddress = new Uri(builder.Configuration["BaseAddress"]));
+builder.Services.AddScoped<IMicrologixPlcService, MicrologixPlcService>();
 builder.Services.AddMudServices();
 builder.Services.AddMediatR(typeof(Program));
 //builder.Services.AddSingleton<IApiLinkService, ApiLinkService>();
@@ -26,14 +28,6 @@ builder.Services.AddCors(options =>
         b => b.AllowAnyMethod()
         .AllowAnyHeader()
         .AllowAnyOrigin());
-});
-
-builder.Services.AddScoped(sp =>
-{
-    var context = sp.GetRequiredService<IHttpContextAccessor>().HttpContext;
-    var client = new HttpClient { BaseAddress = new Uri($"{context!.Request.Scheme}://{context.Request.Host}{context.Request.PathBase}") };
-
-    return client;
 });
 
 // Add services to the container.
@@ -60,13 +54,5 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 app.MapControllers();
 
-using (var scope = app.Services.CreateScope())
-{
-    var notificationService = scope.ServiceProvider.GetService<INotificationService>();
-    if (notificationService is InMemoryNotificationService inmemoryService)
-    {
-        inmemoryService.Preload();
-    }
-}
 
 app.Run();
